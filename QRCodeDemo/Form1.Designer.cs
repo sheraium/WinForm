6f6f06a0aa8e2d3f352bb9357846e3d5cfa79dfc
Chen YongXiu <sheraium@gmail.com>
2018-11-03 23:55:52 +0800

    new QRCodeDemo

--- QRCodeDemo/Form1.Designer.cs
+++ QRCodeDemo/Form1.Designer.cs
@@ -28,13 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtQR = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(245, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtQR
+            // 
+            this.txtQR.Location = new System.Drawing.Point(57, 231);
+            this.txtQR.Name = "txtQR";
+            this.txtQR.Size = new System.Drawing.Size(167, 20);
+            this.txtQR.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(33, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(210, 210);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(340, 266);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtQR);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtQR;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- QRCodeDemo/Form1.cs
+++ QRCodeDemo/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using QRCoder;
 
 namespace QRCodeDemo
 {
@@ -15,6 +16,14 @@ namespace QRCodeDemo
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QRCodeGenerator qrCoder = new QRCodeGenerator();
+            var myData = qrCoder.CreateQrCode(txtQR.Text, QRCodeGenerator.ECCLevel.H);
+            var code = new QRCode(myData);
+            pictureBox1.Image = code.GetGraphic(50);
         }
     }
 }
