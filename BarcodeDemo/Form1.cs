5841ca9ff1a2247c44520ed0312616d84db5adeb
Chen YongXiu <sheraium@gmail.com>
2018-11-04 09:38:56 +0800

    Add BarcodeDemo

---
+++ BarcodeDemo/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BarcodeDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var barCode = textBox1.Text;
+            try
+            {
+                var brCode = Zen.Barcode.BarcodeDrawFactory.Code128WithChecksum;
+                pictureBox1.Image = brCode.Draw(barCode, 60);
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
