e29aa2ae8c4ac3a12c8f87da928122161ae438c0
Chen YongXiu <sheraium@gmail.com>
2018-12-09 18:20:09 +0800

    add AeroWizardDemo

---
+++ AeroWizardDemo/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace AeroWizardDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wizardControl1 = new AeroWizard.WizardControl();
+            this.wizardPage1 = new AeroWizard.WizardPage();
+            this.wizardPage2 = new AeroWizard.WizardPage();
+            ((System.ComponentModel.ISupportInitialize)(this.wizardControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // wizardControl1
+            // 
+            this.wizardControl1.BackColor = System.Drawing.Color.White;
+            this.wizardControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wizardControl1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wizardControl1.Location = new System.Drawing.Point(0, 0);
+            this.wizardControl1.Name = "wizardControl1";
+            this.wizardControl1.Pages.Add(this.wizardPage1);
+            this.wizardControl1.Pages.Add(this.wizardPage2);
+            this.wizardControl1.Size = new System.Drawing.Size(498, 247);
+            this.wizardControl1.TabIndex = 0;
+            this.wizardControl1.Text = "Wizard Title";
+            // 
+            // wizardPage1
+            // 
+            this.wizardPage1.Name = "wizardPage1";
+            this.wizardPage1.Size = new System.Drawing.Size(451, 93);
+            this.wizardPage1.TabIndex = 0;
+            this.wizardPage1.Text = "Step 1:";
+            // 
+            // wizardPage2
+            // 
+            this.wizardPage2.Name = "wizardPage2";
+            this.wizardPage2.Size = new System.Drawing.Size(451, 93);
+            this.wizardPage2.TabIndex = 1;
+            this.wizardPage2.Text = "Step 2:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 247);
+            this.Controls.Add(this.wizardControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.wizardControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AeroWizard.WizardControl wizardControl1;
+        private AeroWizard.WizardPage wizardPage1;
+        private AeroWizard.WizardPage wizardPage2;
+    }
+}
+
---
+++ AeroWizardDemo/FormStepWizard.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace AeroWizardDemo
+{
+    partial class FormStepWizard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.stepWizardControl1 = new AeroWizard.StepWizardControl();
+            this.wizardPage1 = new AeroWizard.WizardPage();
+            this.wizardPage2 = new AeroWizard.WizardPage();
+            ((System.ComponentModel.ISupportInitialize)(this.stepWizardControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // stepWizardControl1
+            // 
+            this.stepWizardControl1.BackColor = System.Drawing.Color.White;
+            this.stepWizardControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.stepWizardControl1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stepWizardControl1.Location = new System.Drawing.Point(0, 0);
+            this.stepWizardControl1.Name = "stepWizardControl1";
+            this.stepWizardControl1.Pages.Add(this.wizardPage1);
+            this.stepWizardControl1.Pages.Add(this.wizardPage2);
+            this.stepWizardControl1.Size = new System.Drawing.Size(477, 353);
+            this.stepWizardControl1.StepListFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World);
+            this.stepWizardControl1.TabIndex = 0;
+            this.stepWizardControl1.Text = "Wizard Title";
+            // 
+            // wizardPage1
+            // 
+            this.wizardPage1.Name = "wizardPage1";
+            this.wizardPage1.Size = new System.Drawing.Size(279, 199);
+            this.stepWizardControl1.SetStepText(this.wizardPage1, "Step 1:");
+            this.wizardPage1.TabIndex = 2;
+            this.wizardPage1.Text = "Step 1:";
+            // 
+            // wizardPage2
+            // 
+            this.wizardPage2.Name = "wizardPage2";
+            this.wizardPage2.Size = new System.Drawing.Size(279, 199);
+            this.stepWizardControl1.SetStepText(this.wizardPage2, "Step 2:");
+            this.wizardPage2.TabIndex = 3;
+            this.wizardPage2.Text = "Step 2:";
+            // 
+            // FormStepWizard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 353);
+            this.Controls.Add(this.stepWizardControl1);
+            this.Name = "FormStepWizard";
+            this.Text = "FormStepWizard";
+            ((System.ComponentModel.ISupportInitialize)(this.stepWizardControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AeroWizard.StepWizardControl stepWizardControl1;
+        private AeroWizard.WizardPage wizardPage1;
+        private AeroWizard.WizardPage wizardPage2;
+    }
+}
