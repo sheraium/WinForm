cdae0702e404713992f53bc5142397343f00280c
Chen YongXiu <sheraium@gmail.com>
2020-06-05 22:45:24 +0800

    add WinFormWithCoreDI project, using dotnet Core DI, HttpClientFactory, Logging, Configuration

---
+++ WinFormWithCoreDI/MainForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormWithCoreDI
+{
+    public partial class MainForm : Form
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly HttpClient _httpClient;
+
+        public MainForm(IServiceProvider serviceProvider, IHttpClientFactory httpClientFactory)
+        {
+            _serviceProvider = serviceProvider;
+            _httpClient = httpClientFactory.CreateClient();
+            InitializeComponent();
+        }
+
+        private async void MainForm_Load(object sender, EventArgs e)
+        {
+            var result = await _httpClient.GetStringAsync("https://www.google.com");
+            await Task.Delay(3000);
+            textBox1.Text = result;
+        }
+    }
+}
---
+++ WinFormWithCoreDI/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace WinFormWithCoreDI
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+
+            var provider = services.BuildServiceProvider();
+            var mainForm = provider.GetService<MainForm>();
+            Application.Run(mainForm);
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            var forms = typeof(Program).Assembly.GetExportedTypes()
+                .Where(x => !x.IsAbstract)
+                .Where(x => typeof(Form).IsAssignableFrom(x));
+
+            foreach (var form in forms)
+            {
+                services.AddScoped(form);
+            }
+
+            services.AddHttpClient();
+            services.AddLogging(builder =>
+            {
+                builder.SetMinimumLevel(LogLevel.Trace);
+                builder.AddConsole();
+            });
+        }
+    }
+}
